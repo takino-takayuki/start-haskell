778bd9a74c040755dd0edba74a374c1d2ac7675c
takino-takayuki <takino.t2@gmail.com>
2021-06-19 01:59:08 +0900

    repl test

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -2,3 +2,18 @@ module MyLib (someFunc) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+add :: Int -> Int -> Int
+add x y = x + y
+
+sub :: Int -> Int -> Int
+sub x y = x - y
+
+-- >>> add 4 5
+-- 9
+--
+
+-- | Test
+--
+-- >>> add 6 7
+-- 13
